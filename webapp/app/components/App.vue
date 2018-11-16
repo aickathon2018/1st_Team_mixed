f0dda5cbd7e80e030473f210d32bbb32c7ff03c3
Ivan Tham <pickfire@riseup.net>
2018-11-17 01:15:56 +0800

    feat: add button to take picture

--- webapp/app/components/App.vue
+++ webapp/app/components/App.vue
@@ -1,32 +1,52 @@
 <template>
-    <Page>
-        <ActionBar title="Delively"/>
-        <GridLayout columns="*" rows="*">
-            <Label class="message" :text="msg" col="0" row="0"/>
-        </GridLayout>
-    </Page>
+  <Page>
+    <ActionBar title="Delively" />
+    <StackLayout>
+      <Button text="Take Picture" @tap="takePicture" />
+      <Label :text="img" />
+    </StackLayout>
+  </Page>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        msg: 'Hello World!'
-      }
+import * as camera from 'nativescript-camera'
+
+export default {
+  data () {
+    return {
+      msg: 'Hello World!',
+      img: ''
+    }
+  },
+  methods: {
+    takePicture () {
+      camera
+        .requestPermissions()
+        .then(() => {
+          camera
+            .takePicture({ width: 300, height: 300, keepAspectRatio: true })
+            .then(imageAsset => {
+              this.img = imageAsset
+              console.log(this.img)
+            })
+            .catch(e => console.error(e))
+        })
+        .catch(e => console.error(e))
     }
   }
+}
 </script>
 
 <style scoped>
-    ActionBar {
-        background-color: #53ba82;
-        color: #ffffff;
-    }
+ActionBar {
+  background-color: #53ba82;
+  color: #ffffff;
+}
 
-    .message {
-        vertical-align: center;
-        text-align: center;
-        font-size: 20;
-        color: #333333;
-    }
+.message {
+  vertical-align: center;
+  text-align: center;
+  font-size: 20;
+  color: #333333;
+}
 </style>

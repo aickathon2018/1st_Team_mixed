e51d6e56c4b83767d81ec51af97f814dd3d1f599
Ivan Tham <pickfire@riseup.net>
2018-11-17 21:02:26 +0800

    feat: add body

--- routes/parcel.js
+++ routes/parcel.js
@@ -1,9 +1,19 @@
 var express = require('express')
 var router = express.Router()
 
+/* Temporary data store. */
+parcel = [
+  {
+    id: '75e7cb59',
+    address: '123, Fake Street',
+    receive_by: '2018-02-29',
+    deliver_by: '2017-11-11'
+  }
+]
+
 /* POST parcel page. */
 router.post('/:id/sign', function (req, res, next) {
-  console.log(req.params.id)
+  console.log(req.body)
   res.send({ 'state': 'success' })
 })
 
